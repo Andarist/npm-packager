9ffb3da4115ff0b68fe0dd538b9c7a657cc0986a
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2018-02-07 12:06:29 -0800

    fixing issue with pkg.files

--- packager.js
+++ packager.js
@@ -3,12 +3,12 @@ const fs = require('fs')
 const _ = require('lodash')
 const log = require('npmlog')
 const copy = require('copy')
+const packlist = require('npm-packlist')
+const pathIsInside = require("path-is-inside")
 
 const DEFAULT_DIST = '.'
 
-const readPackageConfig = () => {
-  return require(process.cwd() + '/package.json')
-}
+const readPackageConfig = () => require(process.cwd() + '/package.json')
 
 const getDistDir = (pkg) => {
   const distDir = _.get(pkg, 'directories.dist', DEFAULT_DIST)
@@ -20,34 +20,38 @@ const relativePath = (file)=> {
 }
 
 const createPackageConfig = (pkg, distDir) => {
-  const packageFile = path.join(distDir, 'package.json')
+  const packageFilePath = path.join(distDir, 'package.json')
   // Remove the pkg.files because we will copy over the files
   // that you want to include on the package
   const finalPkgConfig = _.omit(pkg, ['files'])
   const fileData = JSON.stringify(finalPkgConfig, undefined, 2)
 
-  log.info(`creating ${relativePath(packageFile)}`)
-  return fs.writeFileSync(packageFile, fileData)
+  log.info(`creating ${relativePath(packageFilePath)}`)
+  return fs.writeFileSync(packageFilePath, fileData)
 }
 
 const copyFiles = (pkg, distDir) => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     const opts = {}
-    const files = _.get(pkg, 'files', [])
+    const allFiles = await packlist({ path: process.cwd() })
+    const files = allFiles.filter(file => {
+      const filePath = path.join(process.cwd(), file)
+      // Do not copy package.json because we will create it
+      if(file === 'package.json') return false
+      // Do not copy anything inside the `dist` folder
+      if(pathIsInside(filePath, distDir)) return false
+      return true
+    })
 
     copy.each(files, distDir, opts, (err, files) => {
       if (err) return reject(err)
-
-      files.map((file) => {
-        log.info(`creating ${relativePath(file.dest)}`)
-      })
-
+      files.map((file) => log.info(`created ${relativePath(file.dest)}`))
       return resolve(files)
     })
   })
 }
 
-const createDist = (distDir) => {
+const createDistDir = (distDir) => {
   const fileExists = fs.existsSync(distDir)
   if (fileExists) return
   fs.mkdirSync(distDir)
@@ -56,7 +60,7 @@ const createDist = (distDir) => {
 const prepareDist = (pkg, distDir) => {
   return new Promise(async (resolve, reject) => {
     try {
-      createDist(distDir)
+      createDistDir(distDir)
       createPackageConfig(pkg, distDir)
       await copyFiles(pkg, distDir)
       resolve()
@@ -71,11 +75,7 @@ const preparePackage = () => {
   return new Promise(async (resolve) => {
     const pkg = readPackageConfig()
     const distDir = getDistDir(pkg)
-
-    if (distDir !== process.cwd()) {
-      await prepareDist(pkg, distDir)
-    }
-
+    if (distDir !== process.cwd()) await prepareDist(pkg, distDir)
     return resolve({ distDir })
   })
 }
